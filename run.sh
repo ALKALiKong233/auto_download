453fad245e7259f793995f883ecb350248a259e0
ALKALiKong <alikongalk@gmail.com>
2020-03-29 23:39:50 +0800

    build

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+repo sync --force-sync --no-tags --no-clone-bundle
 rm -rf device/xiaomi/dipper
 rm -rf device/xiaomi/sdm845-common
 rm -rf vendor/xiaomi/sdm845-common
@@ -10,3 +11,7 @@ git clone https://github.com/DerpFest-Devices/vendor_xiaomi_sdm845-common.git -b
 git clone https://github.com/DerpFest-Devices/vendor_xiaomi_dipper.git -b ten vendor/xiaomi/dipper
 git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845.git -b lineage-17.1 kernel/xiaomi/dipper
 git clone https://gitlab.com/ALKALiKong/vendor_xiaomi_dipper-firmware.git -b master vendor/xiaomi/dipper-firmware
+. build/envsetup.sh
+lunch derp_dipper-userdebug
+mka installclean
+mka kronic
