7e7fd704bff22d726969d79b9f0dfa2653b0ad03
ALKALiKong <alikongalk@gmail.com>
2020-04-08 09:21:21 +0800

    pick face unlock gay

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
+cd ~/derp
+rm -rf vendor/aosip
+rm -rf frameworks/base
+rm -rf packages/apps/Settings
 repo sync --force-sync --no-tags --no-clone-bundle
-rm -rf device/xiaomi/dipper
-rm -rf device/xiaomi/sdm845-common
-rm -rf vendor/xiaomi/sdm845-common
-rm -rf vendor/xiaomi/dipper
-rm -rf kernel/xiaomi/dipper
-rm -rf vendor/xiaomi/dipper-firmware
-git clone https://github.com/DerpFest-Devices/device_xiaomi_dipper.git -b ten --depth=1 device/xiaomi/dipper
-git clone https://github.com/DerpFest-Devices/device_xiaomi_sdm845-common.git -b ten --depth=1 device/xiaomi/sdm845-common
-git clone https://github.com/DerpFest-Devices/vendor_xiaomi_sdm845-common.git -b ten --depth=1 vendor/xiaomi/sdm845-common
-git clone https://github.com/DerpFest-Devices/vendor_xiaomi_dipper.git -b ten --depth=1 vendor/xiaomi/dipper
-git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845.git -b lineage-17.1 --depth=1 kernel/xiaomi/dipper
-git clone https://gitlab.com/ALKALiKong/vendor_xiaomi_dipper-firmware.git -b master --depth=1 vendor/xiaomi/dipper-firmware
+cd vendor/aosip
+git fetch https://github.com/PixelExperience/vendor_aosp.git
+git cherry-pick 2428a260dd754c7ec22c031ab2b5c5067bbee721
+cd ~/derp
+cd frameworks/base
+git fetch https://github.com/PixelExperience/frameworks_base.git
+git cherry-pick 6b6d2be4e1166a9828a5b24e84375a493b80068f
+cd ~/derp
+cd packages/apps/Settings
+git fetch https://github.com/PixelExperience/packages_apps_Settings.git
+git cherry-pick 1f59d13d3acee291aa5454900df8f014c320a0e9
+cd ~/derp
 . build/envsetup.sh
 lunch derp_dipper-userdebug
 mka installclean
