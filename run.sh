95db450bb6f64a552e5325fc83e50739a269a166
ALKALiKong <54100342+ALKALiKong233@users.noreply.github.com>
2020-04-12 14:54:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 cd ~/derp
 repo sync --force-sync --no-tags --no-clone-bundle -j8
 cd vendor/aosip
-git fetch https://github.com/amanrajOO7/platform_vendor_aosip
+git fetch https://github.com/amanrajOO7/platform_vendor_aosip.git
 git cherry-pick 302f358d4b873812bfde463e8cb924b77b354a1c
 cd ~/derp
 cd frameworks/base
-git fetch https://github.com/amanrajOO7/platform_frameworks_base
-git cherry-pick bb32df66319da26f8872b366b2a7a1ebb937fee4
+git fetch https://github.com/amanrajOO7/platform_frameworks_base.git
+git cherry-pick 9cb8a536e81d5495ec5b3c6c56851612388aedd1
 cd ~/derp
 cd packages/apps/Settings
-git fetch https://github.com/amanrajOO7/platform_packages_apps_Settings
+git fetch https://github.com/amanrajOO7/platform_packages_apps_Settings.git
 git cherry-pick bb07789260b481486297ba47067b626b673cdfde
 cd ~/derp
 . build/envsetup.sh
