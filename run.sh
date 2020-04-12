972eae6b522e87d24ac5b786a8bd058897e3cae3
ALKALiKong <54100342+ALKALiKong233@users.noreply.github.com>
2020-04-12 10:28:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,20 +2,20 @@ cd ~/derp
 rm -rf vendor/aosip
 rm -rf frameworks/base
 rm -rf packages/apps/Settings
-repo sync --force-sync --no-tags --no-clone-bundle
+repo sync --force-sync --no-tags --no-clone-bundle -j8
 cd vendor/aosip
-git fetch https://github.com/ALKALiKong233/platform_vendor_aosip.git
-git cherry-pick 1dc6f4d4c811624a0c01aed2150e1d57ecd42744
+git fetch https://github.com/amanrajOO7/platform_vendor_aosip
+git cherry-pick 7fd2b51a4d881dbced5e9ab90b6a65841926dd9a
 cd ~/derp
 cd frameworks/base
-git fetch https://github.com/PixelExperience/frameworks_base.git
-git cherry-pick 6b6d2be4e1166a9828a5b24e84375a493b80068f
+git fetch https://github.com/amanrajOO7/platform_frameworks_base
+git cherry-pick bb32df66319da26f8872b366b2a7a1ebb937fee4
 cd ~/derp
 cd packages/apps/Settings
-git fetch https://github.com/PixelExperience/packages_apps_Settings.git
-git cherry-pick 1f59d13d3acee291aa5454900df8f014c320a0e9
+git fetch https://github.com/amanrajOO7/platform_packages_apps_Settings
+git cherry-pick bb07789260b481486297ba47067b626b673cdfde
 cd ~/derp
 . build/envsetup.sh
-lunch derp_dipper-userdebug
+lunch derp_dipper-user
 mka installclean
 mka kronic
